526db40aff3665c3233465b1464ce35a81296957
DanJohan <bestbuysystems01@gmail.com>
2018-07-30 17:48:06 +0530

    admin dashboard count impl

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -4,6 +4,14 @@
 	class Dashboard extends MY_Controller {
 		public function __construct(){
 			parent::__construct();
+
+			$this->load->model('admin/DriverModel');
+			$this->load->model('admin/WorkshopModel');
+
+			$this->load->model('UserModel');
+			$this->load->model('ServiceEnquiryModel');
+			$this->load->model('CarModel');
+
 			if (!$this->session->userdata['is_admin_login'] == TRUE)
 			{
 			   redirect('admin/auth/login'); //redirect to login page
@@ -11,6 +19,14 @@
 		}
 
 		public function index(){
+
+			$data=array();
+			$criteria['returnType'] = 'count';
+			$data['driverCount'] =  $this->DriverModel->search($criteria);
+			$data['userCount'] =  $this->UserModel->search($criteria);	
+			$data['workshopCount'] =  $this->WorkshopModel->search($criteria);
+			$data['enquiryCount'] =  $this->ServiceEnquiryModel->search($criteria);	
+			
 			$data['view'] = 'admin/dashboard/index';
 			$this->load->view('admin/layout', $data);
 		}
